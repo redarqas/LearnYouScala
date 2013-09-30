29fcd7ba514418554d1a3355e4212a2edc42923d
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2013-09-30 10:49:01 +0200

    Ordered Trait

--- ch06/ch06.scala
+++ ch06/ch06.scala
@@ -1,4 +1,4 @@
-class Rational(n: Int, d:Int) {
+class Rational(n: Int, d:Int) extends Ordered[Rational] {
     require(d != 0)
     
     private val g = gcd(n.abs, d.abs)
@@ -20,26 +20,23 @@ class Rational(n: Int, d:Int) {
 
     def + (i: Int): Rational =
     new Rational(numer + i * denom, denom)
-  def - (that: Rational): Rational =
-    new Rational(
-      numer * that.denom - that.numer * denom,
-      denom * that.denom
-    )
-  def - (i: Int): Rational =
-    new Rational(numer - i * denom, denom)
-  def * (that: Rational): Rational =
-    new Rational(numer * that.numer, denom * that.denom)
-  def * (i: Int): Rational =
-    new Rational(numer * i, denom)
-  def / (that: Rational): Rational =
-    new Rational(numer * that.denom, denom * that.numer)
-  def / (i: Int): Rational =
-    new Rational(numer, denom * i)
+    def - (that: Rational): Rational =
+      new Rational(
+        numer * that.denom - that.numer * denom,
+        denom * that.denom
+     )
+    def - (i: Int): Rational = new Rational(numer - i * denom, denom)
+    
+    def * (that: Rational): Rational = new Rational(numer * that.numer, denom * that.denom)
+    
+    def * (i: Int): Rational = new Rational(numer * i, denom)
+    
+    def / (that: Rational): Rational = new Rational(numer * that.denom, denom * that.numer)
+    
+    def / (i: Int): Rational = new Rational(numer, denom * i)
 
-    def < (that: Rational) : Boolean = {
-        numer * that.denom < that.numer * denom
-    }
-
+    def compare(that: Rational) = (this.numer * that.denom) - (that.numer * this.denom)
+    
     def max(that: Rational) = {
         if (this < that) that else this
     }
